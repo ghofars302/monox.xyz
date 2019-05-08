cfd38f624d77165c47801455bb3d26d6b35bc784
Monox <ghofarsuprapto@gmail.com>
2019-05-08 12:12:13 +0700

    yeet fix

--- userscript/davinsurance.user.js
+++ userscript/davinsurance.user.js
@@ -3,29 +3,41 @@
 // @version  1
 // @grant    none
 // @author	 monox
-// @homepageURL  https://monox.xyz/davinsurance.html
+// @homepageURL https://monox.xyz/devinsurance.html
 // @description Skip iklan gan
 // @include https://davinsurance.com/*
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js
 // ==/UserScript==
 
-$.extend({
-  redirectPost: function(location, args) {
-   	var form = '';
-    $.each(args, function(key, value) {
-     form += '<input type="hidden" name="'+key+'" value="'+value+'">';
-    });
-    $('<form action="'+location+'" method="POST">'+form+'</form>').appendTo('body').submit();
-  }
-});
+if (!$.redirectPost) { // Polyfill
+    $.extend({
+        redirectPost: function(location, args) {
+            var form = $("<form></form>");
+            form.attr("method", "post");
+            form.attr("action", location);
 
-var url = $('form[method="post"]')[0] && $('form[method="post"]')[0].action || null
-var token = $('input[type="hidden"]')[0] && $('input[type="hidden"]')[0].value || null
+            $.each(args, function(key, value) {
+                var field = $('<input></input>');
+
+                field.attr("type", "hidden");
+                field.attr("name", key);
+                field.attr("value", value);
+
+                form.append(field);
+            });
+
+            $(form).appendTo('body').submit();
+        }
+    })
+}
+
+var url = $('form[method="post"]')[0] && $('form[method="post"]')[0].action || null;
+var token = $('input[type="hidden"]')[0] && $('input[type="hidden"]')[0].value || null;
 
 if (url && token) {
-	$.redirectPost(url, { get: token })
+    $.redirectPost(url, { get: token });
 } else {
-	eval($('script[type="text/javascript"]')['3'].text)
-	changeLink()
-	window.close()
+    var evaledCode = $('script[type="text/javascript"]')['3'].text.replace("changeLink", "c").replace('window.open(a,"_blank")', "location.href = a") // Redirect thing
+    eval(evaledCode);
+    c();
 }
